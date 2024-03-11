f57fa8dfdef0d9278bcebba50a6d986c6a0570d1
Mixarik <mixarikhh@gmail.com>
2024-03-11 16:32:42 +0100

    update,delete request

--- controllers/car.controller.js
+++ controllers/car.controller.js
@@ -25,19 +25,37 @@ class carController {
    }
 
    async getOneCar(req, res) {
-      const id = req.params.id;
       try {
-         const CarById = await Car.findAll({
-            where: {
-               car_id: id
-            }
-         });
+         const CarById = await Car.findByPk(req.params.id);
          res.status(201).json(CarById)
       } catch (e) {
          console.error(e);
-         res.status(500).json({message: 'Error when getting the cars'});
+         res.status(500).json({message: 'Error when getting a car'});
       }
    }
+
+   async updateCar(req, res) {
+      try {
+         await Car.update(req.body, {where: {car_id: req.params.id}});
+
+         res.status(201).json([await Car.findByPk(req.params.id), {message: `car_id: ${req.params.id} was updated`}])
+      } catch (e) {
+         console.error(e);
+         res.status(500).json({message: 'Error when updating a car'});
+      }
+   }
+
+   async deleteCar(req, res) {
+      try {
+         await Car.destroy({where: {car_id: req.params.id}});
+
+         res.status(201).json({message: `car_id:${req.params.id} was removed`})
+      } catch (e) {
+         console.error(e);
+         res.status(500).json({message: 'Error when removing a car'});
+      }
+   }
+
 
 }
 
--- routes/car-router.js
+++ routes/car-router.js
@@ -11,4 +11,8 @@ router.get('/get-cars/:id', useController.getOneCar);
 
 router.post('/create-new-car', useController.createCar);
 
+router.put('/update-car/:id', useController.updateCar);
+
+router.delete('/delete-car/:id',useController.deleteCar);
+
 module.exports = router;
